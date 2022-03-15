2d38284154a1f9c2eddb1211254420b79191b9db
danaleight <danaleight2000@tutanota.com>
2022-03-15 11:30:05 +0100

    première page html - début

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Share My House</title>
+		<link rel="stylesheet" href="css/general.css">
+	</head>
+	<body>
+		<header>
+			<div class="banner-top">
+				<a href="index.php"><img class="logo logo-img" src="ressources/logo.png" width="48" height="48"></a>
+				<a href=""><img class="logo-img" src="ressources/search.jpg" width="48" heigh="48"></a>
+			</div>
+			<div class="banner-mid-bot">
+				
+			</div>
+		</header>
+	</body>
+</html>
