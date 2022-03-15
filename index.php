c04fb2758e3bf3a43286503e8467d8bcb9b62cb2
danaleight <danaleight2000@tutanota.com>
2022-03-15 12:27:51 +0100

    debut page d'accuei

--- index.php
+++ index.php
@@ -8,11 +8,29 @@
 	<body>
 		<header>
 			<div class="banner-top">
-				<a href="index.php"><img class="logo logo-img" src="ressources/logo.png" width="48" height="48"></a>
+				<a href="index.php"><img class="logo logo-img" alt="website-logo" title="page d'accueil" src="ressources/logo.png" width="48" height="48"></a>
+				<a class="button" href="">Louer votre bien</a>
 				<a href=""><img class="logo-img" src="ressources/search.jpg" width="48" heigh="48"></a>
 			</div>
-			<div class="banner-mid-bot">
-				
+			<div class="banner-bot">
+				<form method="get">
+					<div class="form-destination">
+						<h3>Destination</h3>
+						<input type="text" name="user_destination" placeholder="Où voulez-vous aller ?">
+					</div class="form-arrival">
+					<div>
+						<h3>Arrivée</h3>
+						<input type="date" name="user_arrival" placeholder="Quand arrivez-vous ?">
+					</div>
+					<div class="form-departure">
+						<h3>Départ</h3>
+						<input type="date" name="user_departure" placeholder="Jusque quand ?">
+					</div>
+					<div class="form-people">
+						<h3>Nombre de personne</h3>
+						<input type="number" name="user_people" value="1" min="1" max="16">
+					</div>
+				</form>
 			</div>
 		</header>
 	</body>
